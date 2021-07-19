3b97fb7f89c207204564587a7adbacbf959f5a1e
grvdshwl <79152383+grvdshwl@users.noreply.github.com>
2021-07-19 08:28:08 +0530

    adding session storage for cart

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,9 +1,17 @@
-import {combineReducers} from "redux";
+import { combineReducers } from "redux";
 import cartReducer from "./cart/cart.reducer";
 import userReducer from "./user/user.reducer";
+import { persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
+const persistConfig = {
+  key: "root",
+  storage,
+  whitelist: ["cart"],
+};
 
-export default combineReducers({
-    user:userReducer,
-    cart:cartReducer
+const rootReducer = combineReducers({
+  user: userReducer,
+  cart: cartReducer,
 });
+export default persistReducer(persistConfig, rootReducer);
