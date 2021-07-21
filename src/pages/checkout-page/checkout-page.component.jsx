7396d0ea6866def40f4f9478dd7a33993b21b184
grvdshwl <79152383+grvdshwl@users.noreply.github.com>
2021-07-21 08:59:18 +0530

    Adding stripe integration

--- src/pages/checkout-page/checkout-page.component.jsx
+++ src/pages/checkout-page/checkout-page.component.jsx
@@ -1,57 +1,59 @@
 import React from "react";
 
 import "./checkout-page.styles.scss";
-import {createStructuredSelector} from "reselect";
-import {connect} from "react-redux";
-import { selectCartItems, selectCartTotal } from "../../redux/cart/cart.selectors";
+import { createStructuredSelector } from "reselect";
+import { connect } from "react-redux";
+import {
+  selectCartItems,
+  selectCartTotal,
+} from "../../redux/cart/cart.selectors";
 import CheckoutItem from "../../components/checkout-item/checkout-item.component";
+import StripeButton from "../../components/stripe-button/stripe-button.component";
 
-const CheckoutPage = ({cartItems,total}) =>(
-    <div className="checkout-page">
-        <div className="checkout-header">
+const CheckoutPage = ({ cartItems, total }) => (
+  <div className="checkout-page">
+    <div className="checkout-header">
+      <div className="header-block">
+        <span>Product</span>
+      </div>
 
-            <div className="header-block">
-             <span>
-                Product
-             </span>
-            </div>
+      <div className="header-block">
+        <span>Description</span>
+      </div>
 
-            <div className="header-block">
-             <span>
-                 Description
-             </span>
-            </div>
+      <div className="header-block">
+        <span>Quantity</span>
+      </div>
 
-            <div className="header-block">
-             <span>
-                 Quantity
-             </span>
-            </div>
+      <div className="header-block">
+        <span>Price</span>
+      </div>
 
-            <div className="header-block">
-             <span>
-                 Price
-             </span>
-            </div>
-
-            <div className="header-block">
-             <span>
-                 Remove
-             </span>
-            </div>
-
-        </div>
-
-        {cartItems.length ? cartItems.map((cartItem)=> <CheckoutItem cartItem={cartItem}/>):null}
-        <div className="total">
-            Total : ${total}
-        </div>
+      <div className="header-block">
+        <span>Remove</span>
+      </div>
     </div>
-)
+
+    {cartItems.length
+      ? cartItems.map((cartItem) => (
+          <CheckoutItem key={cartItem.id} cartItem={cartItem} />
+        ))
+      : null}
+    <div className="total">Total : ${total}</div>
+    <div className="test-warning">
+      *Please use the following test credit cards for payments*
+      <br />
+      4242 4242 4242 4242 - Exp: 01/30 - CVV: 158
+      <br />
+      5555 5555 5555 4444 - Exp: 08/32 - CVV: 297
+    </div>
+    <StripeButton price={total} />
+  </div>
+);
 
 const mapStateToProps = createStructuredSelector({
-    cartItems:selectCartItems,
-    total:selectCartTotal
-})
+  cartItems: selectCartItems,
+  total: selectCartTotal,
+});
 
 export default connect(mapStateToProps)(CheckoutPage);
--- src/pages/shop/shop.component.js
+++ src/pages/shop/shop.component.js
@@ -1,8 +1,7 @@
 import React from "react";
 import CollectionsOverview from "../../components/collections-overview/collections-overview.components";
 import { Route } from "react-router-dom";
-import { connect } from "react-redux";
-import { selectCollection } from "../../redux/shop/shop.selector";
+
 import "./shop.styles.scss";
 import CollectionPage from "../collection/collection.component";
 const ShopPage = ({ match }) => {
@@ -18,7 +17,5 @@ const ShopPage = ({ match }) => {
   );
 };
 
-const mapStateToProps = (state, ownProps) => ({
-  collection: selectCollection(ownProps.match.params.collectionId),
-});
+
 export default ShopPage;
